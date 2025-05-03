ac1f9706284f4055197cfcc8479cd583b027e8dd
Tonay <168505740+tonaySadhukhan@users.noreply.github.com>
2025-05-03 11:00:21 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,41 @@
+const youtubedl = require('youtube-dl-exec');
+const express= require('express');
+const fs = require('fs');
+const app=express();
+
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
+app.post('/download', (req, res) => {
+    const url= req.body.url;
+    console.log(url);
+    if (!url) {
+        return res.status(400).send('URL is required');
+      }
+      console.log('Downloading video from URL:', url);
+    const outputPath = '.\\presentation\\video1.mp4';
+    youtubedl(url, {
+        output: outputPath,
+        format: 'mp4',
+      }).then(output => {
+        console.log('Downloaded successfully');
+         res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+        res.setHeader('Content-Type', 'video/mp4');
+        const fileStream = fs.createReadStream(outputPath);
+        fileStream.pipe(res);
+        fileStream.on('close', () => {
+            fs.unlink(outputPath, () => {}); // Optional: delete file after sending
+        });
+      }).catch(err => {
+        console.error('Download failed:', err);
+        res.status(500).send('Download failed',);    
+      }); // Assuming the URL is sent in the request body
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
