b4ea7b3547bd38269126be41692bc22fa2f894cd
Tonay <168505740+tonaySadhukhan@users.noreply.github.com>
2025-05-03 11:20:28 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -19,7 +19,7 @@ app.post('/download', (req, res) => {
     youtubedl(url, {
         output: outputPath,
         format: 'mp4',
-        cookie: './cookies.txt'
+        cookies: "./cookies.txt"
       }).then(output => {
         console.log('Downloaded successfully');
          res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
